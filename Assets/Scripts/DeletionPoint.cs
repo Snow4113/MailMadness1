34b1768b28eb7f66e49370a728d7fd45b7cf1d3b
21KianG <115715072+21KianG@users.noreply.github.com>
2024-06-03 17:53:12 +0100

    Audio into FMOD

--- Assets/Scripts/DeletionPoint.cs
+++ Assets/Scripts/DeletionPoint.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class DeletionPoint : MonoBehaviour
 {
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Package1"))
