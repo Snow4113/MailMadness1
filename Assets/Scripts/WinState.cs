5db72aa7d268819c9d4aa47b774f0969e6cbd45c
Snow4113 <Drajiah4113@gmail.com>
2024-06-06 23:13:30 +0100

    no

--- Assets/Scripts/WinState.cs
+++ Assets/Scripts/WinState.cs
@@ -1,8 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class WinState : MonoBehaviour
 {
-  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
