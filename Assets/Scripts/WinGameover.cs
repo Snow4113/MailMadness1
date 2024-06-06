c41b2ce38399d338b315cddf0fe677349cc2d8ac
joel6760 <joelgoodwin01@gmail.com>
2024-06-06 22:32:57 +0100

    push

---
+++ Assets/Scripts/WinGameover.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WinGameover : MonoBehaviour
+{
+    public Text scoreText;
+
+    If(scoreText <=-5)
+    {
+
+    }
+
+
+
+
+
+   public void RestartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    
+}
