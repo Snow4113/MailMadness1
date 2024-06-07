c72f44d62960d5ae957646953f7031e17e704c38
21KianG <115715072+21KianG@users.noreply.github.com>
2024-06-07 11:15:19 +0100

    lets try this

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -6,7 +6,13 @@ using FMODUnity;
 
 public class ScoreController : MonoBehaviour
 {
+<<<<<<< HEAD
     //[serializedfield] private EventReference ScoreUp;
+=======
+    [SerializeField] private EventReference ScoreUp;
+    [SerializeField] public GameObject gameOver;
+    public static ScoreController instance;
+>>>>>>> 1232ae1a4caacbf574588484bbc39ddf9410ea19
 
     public Text scoreText;
     public Text highscoreText;
@@ -14,13 +20,35 @@ public class ScoreController : MonoBehaviour
     public int score = 0;
     int highscore = 0;
 
-    private void Awake()
+
+    //public void GameOver()
+    
+        //GameOverScreen.Setup(score <= -5);
+    
+
+    public void Update()
     {
+<<<<<<< HEAD
     }
 
 
 
     void Start()
+=======
+        if (score >= -5)
+        {
+            GameOverScreen(!gameOver.activeInHierarchy);
+        }
+    }
+    public void GameOverScreen(bool status)
+    {
+        //If status == true pause | if status == false unpause
+        gameOver.SetActive(status);
+    }
+
+
+        void Start()
+>>>>>>> 1232ae1a4caacbf574588484bbc39ddf9410ea19
     {
         scoreText.text = "Points: " + score.ToString();
         highscoreText.text = "Highscore: " + highscore.ToString();
